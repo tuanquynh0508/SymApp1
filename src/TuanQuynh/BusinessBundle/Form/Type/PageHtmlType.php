44b5899df4677410daedfbfddd2f3f61eacaaa71
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2015-07-21 09:00:56 +0700

    The FormTypeInterface::setDefaultOptions() method is deprecated since version 2.7

--- src/TuanQuynh/BusinessBundle/Form/Type/PageHtmlType.php
+++ src/TuanQuynh/BusinessBundle/Form/Type/PageHtmlType.php
@@ -5,7 +5,7 @@ namespace TuanQuynh\BusinessBundle\Form\Type;
 use TuanQuynh\BusinessBundle\Form\Type\BaseFormType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PageHtmlType extends BaseFormType
 {
@@ -27,7 +27,7 @@ class PageHtmlType extends BaseFormType
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'TuanQuynh\BusinessBundle\Entity\PageHtml',
